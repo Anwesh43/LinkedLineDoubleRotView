8b8fe9baf69f7b5133f7d3096dc5ed2e84733ce4
Anwesh43 <anweshthecool0@gmail.com>
2018-07-24 09:52:18 +0530

    adding LineDoubleRotView class

--- linedoublerotview/src/main/java/com/anwesh/uiprojects/linedoublerotview/LineDoubleRotView.kt
+++ linedoublerotview/src/main/java/com/anwesh/uiprojects/linedoublerotview/LineDoubleRotView.kt
@@ -3,3 +3,27 @@ package com.anwesh.uiprojects.linedoublerotview
 /**
  * Created by anweshmishra on 24/07/18.
  */
+
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+
+val NODES : Int = 5
+
+class LineDoubleRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
