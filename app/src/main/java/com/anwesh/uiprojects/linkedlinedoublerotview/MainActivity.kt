5068ce2088083b2462eb9754db3c264927d6b53d
Anwesh43 <anweshthecool0@gmail.com>
2018-07-24 10:21:59 +0530

    adding LineDoubleRotView in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedlinedoublerotview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlinedoublerotview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedlinedoublerotview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linedoublerotview.LineDoubleRotView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LineDoubleRotView.create(this)
     }
 }
